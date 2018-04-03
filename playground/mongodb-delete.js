fbc72b18e3e85d486385722e05bc67403dc60d16
Peter Yu <PeterYu@gmail.com>
2018-04-03 18:28:01 -0400

    added delete file into playground

---
+++ playground/mongodb-delete.js
@@ -0,0 +1,26 @@
+// const MongoClient = require('mongodb').MongoClient;
+const {MongoClient, ObjectID} = require('mongodb');
+
+MongoClient.connect('mongodb://localhost:27017/TodoApp', (err, db) => {
+  if (err) {
+    return console.log('Unable to connect to the MongoDB server');
+  }
+  console.log('Connected to MongoDB server');
+
+  //deleteMany
+  db.collection('Users').deleteMany({name: 'Peter'}).then((result) => {
+    console.log(result);
+  });
+
+  // deleteOne
+  // db.collection('Users').deleteOne({text: 'Eat lunch'}).then((result) => {
+  //   console.log(result);
+  // });
+
+  // findOneAndDelete
+  db.collection('Users').findOneAndDelete({_id: new ObjectID('5ac3c2f7862e3c1c6862caab')}).then((result) => {
+    console.log(result)
+  });
+
+  // db.close();
+});
